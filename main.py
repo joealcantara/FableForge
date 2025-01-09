cb49cb81a98e1da5944f45eef67d811a7278315e
Joe Alcantara <joe@joealcantara.com>
2025-01-09 20:20:37 +0000

    added adventurer module

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
-import sys
+# This is the main part of the program.
+import adventurer as adv
 
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+def main():
+    bob = adv.Adventurer("bob", 42)
+    print(bob.name)
+    print(bob.age)
+    
 
-
-# Subclass QMainWindow to customize your application's main window
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
-        button.clicked.connect(self.the_button_was_toggled)
-
-        self.setFixedSize(QSize(400, 300))
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
-
-    def the_button_was_clicked(self):
-        print("Clicked!")
-
-    def the_button_was_toggled(self, checked):
-        print("Checked?", checked)
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec()
+if __name__ == "__main__":
+    main()
