b74cc66076af2e4b18cb4aad609d6f7b2cdfd70f
Joe Alcantara <joe@joealcantara.com>
2025-01-12 13:56:13 +0000

    added window to the game and refactored code

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-# This is the main part of the program.
+# IMPORTS
+# Imports from Python Standard Libraries
+import pygame, random, sys
+
+# Imports from other python scripts
 import adventurer as adv
-import random
+from settings import Settings
+
+class FableForge():
+    """
+    Main Game Initialisation
+    """
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("FableForge")
+
+    def run_game(self):
+        running = True
+        while running:
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print('Game Ended')
+                running = False
+                sys.exit()
+
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_colour)
+        pygame.display.update()
 
 
 def rollDice(sides):
@@ -29,7 +66,9 @@ def close_logger(log):
     log.close()
 
 
-def main():
+
+
+
     log = create_logger()
     bob = createRandomCharacter('Bob')
     joe = createRandomCharacter('Joe', 'Warrior')
@@ -38,4 +77,5 @@ def main():
     close_logger(log)
 
 if __name__ == "__main__":
-    main()
+    ff = FableForge()
+    ff.run_game()
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """
+    A Class to manage settings for the FableForge Game.
+    """
+    def __init__(self):
+        """
+        Intialise the Settings for the FableForge Game.
+        """
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = ("WHITE")
