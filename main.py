c2dddfda2331c05fcc42fb1ee58b30c42de63c05
Joe Alcantara <joe@joealcantara.com>
2025-01-12 14:22:37 +0000

    added engine and logger files, tested logger.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame, random, sys
 
 # Imports from other python scripts
 import adventurer as adv
+from logger import Logger
+import engine
 from settings import Settings
 
 class FableForge():
@@ -14,6 +16,7 @@ class FableForge():
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
+        self.log = Logger()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("FableForge")
@@ -25,56 +28,24 @@ class FableForge():
             self._update_screen()
             self.clock.tick(60)
 
-
-
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                self.log.close_logger()
                 print('Game Ended')
                 running = False
                 sys.exit()
-
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_colour)
         pygame.display.update()
 
-
-def rollDice(sides):
-    return random.randrange(1, sides)
-
-def createRandomCharacter(name, charClass = None):
-    strength = rollDice(20)
-    dexterity = rollDice(20)
-    constitution = rollDice(20)
-    intelligence = rollDice(20)
-    wisdom = rollDice(20)
-    charisma = rollDice(20)
-    age = rollDice(100)
-    if charClass == 'Warrior':
-        return adv.Warrior(name, age, strength, dexterity, constitution, intelligence, wisdom, charisma)
-    else:
-        return adv.Adventurer(name, age, strength, dexterity, constitution, intelligence, wisdom, charisma)
-
-def create_logger():
-    file = open("log.txt", "w+")
-    file.write("Log open\n")
-    return file
-
-def close_logger(log):
-    log.write("Log closed")
-    log.close()
-
-
-
-
-
-    log = create_logger()
-    bob = createRandomCharacter('Bob')
-    joe = createRandomCharacter('Joe', 'Warrior')
-    bob.printStats()
-    joe.printStats()
-    close_logger(log)
+    # log = create_logger()
+    # bob = createRandomCharacter('Bob')
+    # joe = createRandomCharacter('Joe', 'Warrior')
+    # bob.printStats()
+    # joe.printStats()
+    # close_logger(log)
 
 if __name__ == "__main__":
     ff = FableForge()
