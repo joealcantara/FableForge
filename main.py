282c631d069b3d5cc65b3b39c4cc5ea3f870fe8b
Joe Alcantara <joe@joealcantara.com>
2025-08-25 21:12:50 +0100

    test of aliases

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import random
+
+def diceRoll(sides):
+    return random.randint(1, sides)
+
 def main():
     print("Hello from fableforge!")
+    print(diceRoll(20))
 
 
 if __name__ == "__main__":
